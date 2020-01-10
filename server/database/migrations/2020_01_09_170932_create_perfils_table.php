72e57f5fef51dafa04f746a3ac84fa8052322c73
Bruno Gustavo <brugucolosil95@gmail.com>
2020-01-09 22:46:54 -0300

    changed name of server folder

---
+++ server/database/migrations/2020_01_09_170932_create_perfils_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePerfilsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perfils', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->unique();
+            $table->string('label');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perfils', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
+    }
+}
